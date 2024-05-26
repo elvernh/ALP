98a58438d46af858a08bb48f1f1c14053dccff93
elvernh <87465378+elvernh@users.noreply.github.com>
2024-05-26 15:54:39 +0700

    modifed Penjual

--- Penjual.java
+++ Penjual.java
@@ -1,3 +1,9 @@
-public class Penjual {
-    
+public class Penjual extends User{
+    protected String role;
+    protected int saldo;
+    public Penjual(String name){
+        super(name);
+        this.role = "Seller";
+        this.saldo = 0;
+    }
 }
