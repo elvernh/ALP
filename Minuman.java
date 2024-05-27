e8987821e710f655e894f02eaf4e25184f9b070c
elvernh <87465378+elvernh@users.noreply.github.com>
2024-05-27 12:24:15 +0700

    modified makanan

--- Minuman.java
+++ Minuman.java
@@ -1,5 +1,7 @@
 public class Minuman extends Produk {
+    protected String kategori;
     public Minuman(String name, int price) {
         super(name, price);
+        this.kategori = "Minuman";
     }
 }
--- makanan.java
+++ makanan.java
@@ -1,5 +1,7 @@
 public class makanan extends Produk {
+    protected String kategori;
     public makanan(String name, int price) {
        super(name, price);
+       this.kategori = "Makanan";
     }
 }
